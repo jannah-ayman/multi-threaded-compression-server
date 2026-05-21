a586490ceaad20f051a47074b204613c373bb793
Jannah <jannahayman16@gmail.com>
2026-05-21 14:24:25 +0300

    implement client GUI application

--- Client/Client.Designer.cs
+++ Client/Client.Designer.cs
@@ -28,20 +28,96 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.connectBtn = new System.Windows.Forms.Button();
+            this.browseBtn = new System.Windows.Forms.Button();
+            this.logText = new System.Windows.Forms.RichTextBox();
+            this.filePathTxt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sendBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // connectBtn
+            // 
+            this.connectBtn.Location = new System.Drawing.Point(139, 69);
+            this.connectBtn.Name = "connectBtn";
+            this.connectBtn.Size = new System.Drawing.Size(97, 34);
+            this.connectBtn.TabIndex = 0;
+            this.connectBtn.Text = "Connect";
+            this.connectBtn.UseVisualStyleBackColor = true;
+            this.connectBtn.Click += new System.EventHandler(this.connectBtn_Click);
+            // 
+            // browseBtn
+            // 
+            this.browseBtn.Enabled = false;
+            this.browseBtn.Location = new System.Drawing.Point(139, 158);
+            this.browseBtn.Name = "browseBtn";
+            this.browseBtn.Size = new System.Drawing.Size(97, 34);
+            this.browseBtn.TabIndex = 1;
+            this.browseBtn.Text = "Browse Files";
+            this.browseBtn.UseVisualStyleBackColor = true;
+            this.browseBtn.Click += new System.EventHandler(this.browseBtn_Click);
+            // 
+            // logText
+            // 
+            this.logText.Location = new System.Drawing.Point(370, 69);
+            this.logText.Name = "logText";
+            this.logText.Size = new System.Drawing.Size(351, 304);
+            this.logText.TabIndex = 2;
+            this.logText.Text = "";
+            // 
+            // filePathTxt
+            // 
+            this.filePathTxt.Location = new System.Drawing.Point(159, 260);
+            this.filePathTxt.Name = "filePathTxt";
+            this.filePathTxt.Size = new System.Drawing.Size(162, 22);
+            this.filePathTxt.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 260);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Selected file:";
+            // 
+            // sendBtn
+            // 
+            this.sendBtn.Enabled = false;
+            this.sendBtn.Location = new System.Drawing.Point(139, 339);
+            this.sendBtn.Name = "sendBtn";
+            this.sendBtn.Size = new System.Drawing.Size(97, 34);
+            this.sendBtn.TabIndex = 5;
+            this.sendBtn.Text = "Send File";
+            this.sendBtn.UseVisualStyleBackColor = true;
+            this.sendBtn.Click += new System.EventHandler(this.sendBtn_Click);
             // 
             // Client
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.sendBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filePathTxt);
+            this.Controls.Add(this.logText);
+            this.Controls.Add(this.browseBtn);
+            this.Controls.Add(this.connectBtn);
             this.Name = "Client";
             this.Text = "Client";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button connectBtn;
+        private System.Windows.Forms.Button browseBtn;
+        private System.Windows.Forms.RichTextBox logText;
+        private System.Windows.Forms.TextBox filePathTxt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button sendBtn;
     }
 }
 
--- Client/Client.cs
+++ Client/Client.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Net.Sockets;
 using System.Windows.Forms;
 
 namespace Client
@@ -15,6 +10,105 @@ namespace Client
         public Client()
         {
             InitializeComponent();
+        }
+        string selectedFilePath = "";
+        TcpClient tcpClient;
+        NetworkStream ns;
+        private async void connectBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                connectBtn.Enabled = false;
+                logText.Text += "Connecting..." + Environment.NewLine;
+
+                tcpClient = new TcpClient();
+                await tcpClient.ConnectAsync("127.0.0.1", 9050);
+                ns = tcpClient.GetStream();
+
+                logText.Text += "Connected to server." + Environment.NewLine;
+                browseBtn.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                logText.Text += $"Error: {ex.Message}" + Environment.NewLine;
+                connectBtn.Enabled = true;
+            }
+        }
+
+        private void browseBtn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                selectedFilePath = dlg.FileName;
+                filePathTxt.Text = selectedFilePath;
+                logText.Text += "File selected." + Environment.NewLine;
+                sendBtn.Enabled = true;
+            }
+        }
+
+        private async void sendBtn_Click(object sender, EventArgs e)
+        {
+            if (selectedFilePath == "")
+            {
+                MessageBox.Show("Please select a file first.");
+                return;
+            }
+
+            if (tcpClient == null || !tcpClient.Connected)
+            {
+                MessageBox.Show("Please connect to the server first.");
+                return;
+            }
+
+            sendBtn.Enabled = false;
+            browseBtn.Enabled = false;
+
+            try
+            {
+                // STEP 1: Read file into memory
+                byte[] fileData = File.ReadAllBytes(selectedFilePath);
+                long fileSize = fileData.Length;
+                logText.Text += $"Sending file... ({fileSize} bytes)" + Environment.NewLine;
+
+                // STEP 2: Send file size then file data
+                byte[] sizeBuf = BitConverter.GetBytes(fileSize);
+                await ns.WriteAsync(sizeBuf, 0, 8);
+                await ns.WriteAsync(fileData, 0, (int)fileSize);
+                await ns.FlushAsync();
+                logText.Text += $"Original: {fileSize} bytes" + Environment.NewLine;
+                logText.Text += "File sent. Waiting for compressed file..." + Environment.NewLine;
+
+                // STEP 3: Read compressed size (8 bytes)
+                byte[] compSizeBuf = new byte[8];
+                int read = await ns.ReadAsync(compSizeBuf, 0, 8);
+                long compSize = BitConverter.ToInt64(compSizeBuf, 0);
+
+                // STEP 4: Read compressed file bytes
+                byte[] compData = new byte[compSize];
+                int totalRead = 0;
+                while (totalRead < compSize)
+                {
+                    read = await ns.ReadAsync(compData, totalRead, (int)(compSize - totalRead));
+                    if (read == 0) throw new Exception("Connection closed unexpectedly.");
+                    totalRead += read;
+                }
+
+                // STEP 5: Save compressed file
+                string savePath = selectedFilePath + ".gz";
+                File.WriteAllBytes(savePath, compData);
+                logText.Text += $"Compressed: {compSize} bytes" + Environment.NewLine;
+                logText.Text += $"Done! Saved as: {savePath}" + Environment.NewLine;
+            }
+            catch (Exception ex)
+            {
+                logText.Text += $"Error: {ex.Message}" + Environment.NewLine;
+            }
+            finally
+            {
+                connectBtn.Enabled = true;
+            }
+
         }
     }
 }
