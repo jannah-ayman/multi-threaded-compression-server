d70447c75b089a0769322f719d9c2335a71c2c42
Jannah <jannahayman16@gmail.com>
2026-05-21 14:05:13 +0300

    implement multi-threaded compression server

--- Server/Server.Designer.cs
+++ Server/Server.Designer.cs
@@ -28,13 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.startBtn = new System.Windows.Forms.Button();
+            this.logText = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(95, 86);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(108, 30);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.Text = "Start Server";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // logText
+            // 
+            this.logText.Location = new System.Drawing.Point(318, 35);
+            this.logText.Name = "logText";
+            this.logText.Size = new System.Drawing.Size(390, 366);
+            this.logText.TabIndex = 1;
+            this.logText.Text = "";
             // 
             // Server
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.logText);
+            this.Controls.Add(this.startBtn);
             this.Name = "Server";
             this.Text = "Server";
             this.ResumeLayout(false);
@@ -42,6 +64,9 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.RichTextBox logText;
     }
 }
 
--- Server/Server.cs
+++ Server/Server.cs
@@ -1,10 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Net.Sockets;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +13,78 @@ namespace Server
         public Server()
         {
             InitializeComponent();
+        }
+
+        TcpListener listener;
+        private static int activeConnections = 0;
+
+        private async void startBtn_Click(object sender, EventArgs e)
+        {
+            listener = new TcpListener(IPAddress.Any, 9050);
+            listener.Start();
+            logText.Text += "Server started on port 9050..." + Environment.NewLine;
+            startBtn.Enabled = false;
+
+            while (true)
+            {
+                TcpClient client = await listener.AcceptTcpClientAsync();
+                _ = HandleClientAsync(client);
+            }
+        }
+
+        private async Task HandleClientAsync(TcpClient client)
+        {
+            NetworkStream ns = client.GetStream();
+            activeConnections++;
+            logText.Text += $"Client connected. Active connections: {activeConnections}" + Environment.NewLine;
+
+            try
+            {
+                // read file size
+                byte[] sizeBuf = new byte[8];
+                int read = await ns.ReadAsync(sizeBuf, 0, 8);
+                long fileSize = BitConverter.ToInt64(sizeBuf, 0);
+
+                logText.Text += $"Expecting file of {fileSize} bytes..." + Environment.NewLine;
+
+                // read file bytes
+                byte[] fileData = new byte[fileSize];
+                int totalRead = 0;
+                while (totalRead < fileSize)
+                {
+                    int r = await ns.ReadAsync(fileData, totalRead, (int)(fileSize - totalRead));
+                    if (r == 0) throw new Exception("Connection closed unexpectedly.");
+                    totalRead += r;
+                }
+                logText.Text += "File received, compressing..." + Environment.NewLine;
+
+                // compress using gzip
+                MemoryStream ms = new MemoryStream();
+                using (GZipStream gz = new GZipStream(ms,CompressionMode.Compress))
+                {
+                    gz.Write(fileData, 0, fileData.Length);
+                }
+                byte[] compressedData = ms.ToArray();
+                logText.Text += $"Compressed size: {compressedData.Length} bytes" + Environment.NewLine;
+
+                // send compressed size then compressed data
+                byte[] compSizeBuf = BitConverter.GetBytes((long)compressedData.Length);
+                await ns.WriteAsync(compSizeBuf, 0, 8);
+                await ns.WriteAsync(compressedData, 0, compressedData.Length);
+                await ns.FlushAsync();
+                logText.Text += "Compressed file sent to client." + Environment.NewLine;
+            }
+            catch (Exception ex)
+            {
+                logText.Text += $"Error: {ex.Message}" + Environment.NewLine;
+            }
+            finally
+            {
+                ns.Close();
+                client.Close();
+                activeConnections--;
+                logText.Text += $"Client disconnected. Active connections: {activeConnections}" + Environment.NewLine;
+            }
         }
     }
 }
